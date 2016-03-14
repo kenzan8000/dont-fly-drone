a57a3165ffe2901867ae2e53c1a49e79ae2b53ab
kenzan8000 <kenzan8000@gmail.com>
2016-03-14 15:05:46 -0700

    add polygon controlelr test

--- server/test/controllers/polygon_controller_test.rb
+++ server/test/controllers/polygon_controller_test.rb
@@ -1,7 +1,20 @@
 require 'test_helper'
 
-class PolygonControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+describe PolygonController do
+  before {
+  }
+
+  it "#GET /polygon - return application_code 400 with no parameters" do
+    get :index
+    res = JSON.parse(response.body)
+    res['application_code'].must_equal 400
+  end
+
+  it "#GET /polygon - return application_code 200 and polygon array" do
+    get :index, :south => 37.7, :north => 37.8, :west => -122.5, :east => -122.4
+    res = JSON.parse(response.body)
+    res['application_code'].must_equal 200
+    res['polygons'].must_be_kind_of Array
+  end
 end
