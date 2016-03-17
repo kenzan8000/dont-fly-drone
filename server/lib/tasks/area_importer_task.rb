99138233b2e09c0b8901bab4f8fbc47cea7f389a
kenzan8000 <kenzan8000@gmail.com>
2016-03-17 15:23:47 -0700

    update README and move geojsons

--- server/lib/tasks/area_importer_task.rb
+++ server/lib/tasks/area_importer_task.rb
@@ -9,11 +9,11 @@ class Tasks::AreaImporterTask
     Polygon.delete_all
 
     # create
-    file_names = [ '5_mile_airport.geojson', 'us_military.geojson', 'us_national_park.geojson' ]
+    file_names = ['5_mile_airport.geojson', 'us_military.geojson', 'us_national_park.geojson']
     area_types = ['airport', 'military', 'national_park']
     area_importer = AreaImporter.new
     (0...file_names.length).each do |i|
-      file = File.read("./../resources/geojsons/#{file_names[i]}")
+      file = File.read("./../third-party/drone-feedback/sources/geojson/#{file_names[i]}")
       json = JSON.parse(file)
       area_importer.import_geojson(json, area_types[i])
     end
